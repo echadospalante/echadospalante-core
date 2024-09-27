fce5e3c51c8fcd89ebbf2dabb040a0780d4894fc
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2024-09-27 13:32:15 -0500

    Se agrega comentario

--- src/app/modules/ventures/category.ts
+++ src/app/modules/ventures/category.ts
@@ -7,5 +7,5 @@ export interface VentureCategory {
   slug: string;
   description: string;
   ventures: Venture[];
-  users: User[];
+  users: User[]; // Se agrega para relación inversa de usuarios con preferencias
 }
