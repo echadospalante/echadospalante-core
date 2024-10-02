e4e7011a37b35327b06b0571de2c1e9bc1ff740f
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2024-10-02 18:32:43 -0500

    Se arregla import

--- src/app/modules/ventures/index.ts
+++ src/app/modules/ventures/index.ts
@@ -2,3 +2,4 @@ export { VentureCategory } from "./category";
 export { VentureDetail } from "./detail";
 export { VentureSubscription } from "./subscription";
 export { Venture } from "./venture";
+export { VentureCategoryCreate } from "./category";
