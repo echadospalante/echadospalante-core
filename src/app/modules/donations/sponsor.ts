6662b3f0863192f3a326cadfff6b7b036306f4a3
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2024-10-27 17:01:56 -0500

    Se hace relacion opcional

--- src/app/modules/donations/sponsor.ts
+++ src/app/modules/donations/sponsor.ts
@@ -4,8 +4,8 @@ import { Venture } from "../ventures/venture";
 
 export interface VentureSponsorship {
   id: string;
-  sponsor: UserDetail;
-  venture: Venture;
+  sponsor?: UserDetail;
+  venture?: Venture;
   monthlyAmount: number;
   createdAt: Date;
 }
